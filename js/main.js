bdd4c8f35574d3936534c3220893e8af689fcfcb
Savana Minerva <savanaminerva@Savanas-MacBook-Pro.local>
2021-10-12 18:13:24 -0400

    added csv

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // write your javascript code here.
 // feel free to change the pre-set attributes as you see fit
 
+/*
 let margin = {
     top: 60,
     left: 50,
@@ -20,3 +21,60 @@ let svg1 = d3.select('#d3-container')
   .style('border', 'solid')
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
+// Set SVG width, height, and padding
+const w = 500;
+const h = 500;
+const padding = 60;
+*/
+
+d3.csv("data.csv", function(error, data) {
+        if (error) {
+            throw error;
+        }
+
+const width = 900;
+const height = 450;
+const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+
+const svg = d3.select('#d3-container')
+  .append('svg')
+  .attr('width', width - margin.left - margin.right)
+  .attr('height', height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
+
+const x = d3.scaleBand()
+  .domain(d3.range(data.length))
+  .padding(0.1)
+
+const y = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height - margin.bottom, margin.top])
+
+svg
+  .append("g")
+  .attr("fill", 'royalblue')
+  .selectAll("rect")
+  .data(data.sort((a, b) => d3.descending(a.score, b.score)))
+  .join("rect")
+    .attr("x", (d, i) => x(i))
+    .attr("y", d => y(d.score))
+    .attr('title', (d) => d.score)
+    .attr("class", "rect")
+    .attr("height", d => y(0) - y(d.score))
+    .attr("width", x.bandwidth());
+
+function yAxis(g) {
+  g.attr("transform", `translate(${margin.left}, 0)`)
+    .call(d3.axisLeft(y).ticks(null, data.format))
+    .attr("font-size", '20px')
+}
+
+function xAxis(g) {
+  g.attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickFormat(i => data[i].name))
+    .attr("font-size", '20px')
+}
+
+svg.append("g").call(xAxis);
+svg.append("g").call(yAxis);
+svg.node();
